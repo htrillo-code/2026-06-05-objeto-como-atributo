8ddd1ed1ee1fdc5853694a6050ff6b17ebceaeec
htrillo-code <h.trillo@alumno.etec.um.edu.ar>
2026-06-05 20:08:29 -0300

    Fecha.kt
    Fecha.kt

---
+++ Fecha.kt
@@ -0,0 +1,4 @@
+package org.example
+class Fecha ( private var dia: Int, private var mes: Int, private var ano: Int){
+    override fun toString():String{
+        return "dia=${dia}, mes=${mes}, año=${ano}"}}
